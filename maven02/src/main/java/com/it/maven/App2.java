b53a4c4c90cade1b4b4b639ea2fe0379423d8b6d
Nick <1060768890@qq.com>
2016-11-30 10:56:16 +0800

    add App1

---
+++ maven02/src/main/java/com/it/maven/App2.java
@@ -0,0 +1,13 @@
+package com.it.maven;
+
+/**
+ * Hello world1!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!1" );
+    }
+}
